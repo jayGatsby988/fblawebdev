863af694158982b0d313a86572cfeae02ef87e50
Pravan123 <speedfnm@gmail.com>
2025-02-01 00:40:10 -0800

    bug fix

---
+++ src/app/nigger/page.tsx
@@ -0,0 +1,28 @@
+
+import Image from "next/image"
+
+
+export default function Page(){
+
+
+
+
+    return(<>
+
+
+
+
+            <img src = "download.jpeg"></img>
+            <img src = "download (1).jpeg"></img>
+
+
+
+
+    
+    
+    
+    
+    </>)
+}
+
+
