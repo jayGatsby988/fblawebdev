89827adf224e128cb2de67c53b88b74a28c7d8c0
Robel <61947070+RobelY123@users.noreply.github.com>
2025-01-31 17:52:48 -0800

    signup fix

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,81 +1,97 @@
-'use client'
+"use client";
 import { useState } from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
-import { FaUser, FaEnvelope, FaLock, FaHome } from "react-icons/fa";
+import { FaEnvelope, FaLock, FaHome } from "react-icons/fa";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithPopup,
+} from "firebase/auth";
+import { redirect, useRouter, useSearchParams } from "next/navigation";
+import { useAuth } from "@/app/context/authcontext";
 
 export default function SignupPage() {
-  const [formData, setFormData] = useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    password: ""
-  });
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const [error, setError] = useState("");
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const search = searchParams.get("redirect");
+  const { user } = useAuth();
+
+  if (user) {
+    if (search) {
+      redirect("/" + search || "");
+    } else {
+      redirect("/");
+    }
+  }
 
   const handleChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
-  const handleSubmit = (e) => {
+  const handleEmailSignUp = async (e) => {
     e.preventDefault();
-    console.log("Form submitted", formData);
+    setError("");
+    try {
+      await createUserWithEmailAndPassword(
+        auth,
+        formData.email,
+        formData.password
+      );
+      if (search) {
+        redirect("/" + search || "");
+      } else {
+        redirect("/");
+      }
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  const handleGoogleSignUp = async () => {
+    setError("");
+    try {
+      await signInWithPopup(auth, provider);
+      redirect("/" + search || "");
+    } catch (error) {
+      setError(error.message);
+    }
   };
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-blue-200 to-blue-300 p-6">
-      <motion.div 
-        initial={{ opacity: 0, y: 50 }} 
-        animate={{ opacity: 1, y: 0 }} 
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5 }}
-        className="bg-white p-20 rounded-3xl shadow-2xl max-w-2xl w-full relative"
+        className="bg-white p-20 rounded-3xl shadow-2xl max-w-lg w-full relative"
       >
-        <Link href="/" className="absolute top-4 left-4 text-blue-700 hover:text-indigo-900 transition text-lg">
+        <Link
+          href="/"
+          className="absolute top-4 left-4 text-blue-700 hover:text-indigo-900 transition text-lg"
+        >
           <FaHome className="inline mr-2" /> Home
         </Link>
-        <h2 className="text-5xl font-extrabold text-blue-700 text-center mb-10">Join Us</h2>
-        <form onSubmit={handleSubmit} className="space-y-6">
-          <div className="flex gap-6">
-            <div className="w-1/2">
-              <label className="text-gray-700 font-medium">First Name</label>
-              <div className="flex items-center border border-gray-300 p-4 rounded-lg bg-gray-100">
-                <FaUser className="text-gray-500" />
-                <input 
-                  type="text" 
-                  name="firstName" 
-                  value={formData.firstName} 
-                  onChange={handleChange} 
-                  placeholder="John" 
-                  className="outline-none bg-transparent w-full pl-2"
-                  required
-                />
-              </div>
-            </div>
-            <div className="w-1/2">
-              <label className="text-gray-700 font-medium">Last Name</label>
-              <div className="flex items-center border border-gray-300 p-4 rounded-lg bg-gray-100">
-                <FaUser className="text-gray-500" />
-                <input 
-                  type="text" 
-                  name="lastName" 
-                  value={formData.lastName} 
-                  onChange={handleChange} 
-                  placeholder="Doe" 
-                  className="outline-none bg-transparent w-full pl-2"
-                  required
-                />
-              </div>
-            </div>
-          </div>
+        <h2 className="text-5xl font-extrabold text-blue-700 text-center mb-6">
+          Sign Up
+        </h2>
+        {error && <p className="text-red-600 text-center mb-4">{error}</p>}
+        <form onSubmit={handleEmailSignUp} className="space-y-6">
           <div>
             <label className="text-gray-700 font-medium">Email</label>
             <div className="flex items-center border border-gray-300 p-4 rounded-lg bg-gray-100">
               <FaEnvelope className="text-gray-500" />
-              <input 
-                type="email" 
-                name="email" 
-                value={formData.email} 
-                onChange={handleChange} 
-                placeholder="johndoe@example.com" 
+              <input
+                type="email"
+                name="email"
+                value={formData.email}
+                onChange={handleChange}
+                placeholder="johndoe@example.com"
                 className="outline-none bg-transparent w-full pl-2"
                 required
               />
@@ -85,27 +101,49 @@ export default function SignupPage() {
             <label className="text-gray-700 font-medium">Password</label>
             <div className="flex items-center border border-gray-300 p-4 rounded-lg bg-gray-100">
               <FaLock className="text-gray-500" />
-              <input 
-                type="password" 
-                name="password" 
-                value={formData.password} 
-                onChange={handleChange} 
-                placeholder="••••••••" 
+              <input
+                type="password"
+                name="password"
+                value={formData.password}
+                onChange={handleChange}
+                placeholder="••••••••"
                 className="outline-none bg-transparent w-full pl-2"
                 required
               />
             </div>
           </div>
-          <motion.button 
+          <motion.button
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
-            type="submit" 
+            type="submit"
             className="w-full bg-cyan-900 text-white py-4 rounded-lg font-bold text-xl hover:bg-indigo-700 transition"
           >
             Sign Up
           </motion.button>
         </form>
-        <p className="text-gray-600 text-center mt-6 text-lg">Already have an account? <Link href="/login" className="text-indigo-700 font-bold hover:underline">Login</Link></p>
+        <div className="mt-6 text-center">
+          <p className="text-gray-700">Or</p>
+          <button
+            onClick={handleGoogleSignUp}
+            className="w-full flex items-center justify-center bg-white border border-gray-300 p-3 rounded-lg font-semibold shadow-sm hover:bg-gray-100 transition mt-2"
+          >
+            <img
+              src="https://registry.npmmirror.com/@lobehub/icons-static-png/latest/files/dark/google-color.png"
+              alt="Google"
+              className="w-5 h-5 mr-2"
+            />
+            Continue with Google
+          </button>
+        </div>
+        <p className="text-gray-600 text-center mt-6 text-lg">
+          Already have an account?{" "}
+          <Link
+            href="/login"
+            className="text-indigo-700 font-bold hover:underline"
+          >
+            Log In
+          </Link>
+        </p>
       </motion.div>
     </div>
   );
